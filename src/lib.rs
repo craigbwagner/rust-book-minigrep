99c23f8192bc2c0a21e05ad031a85a226827aa0f
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-05 16:09:37 -0400

    add test for case insensivity on searches

--- src/lib.rs
+++ src/lib.rs
@@ -19,6 +19,10 @@ impl Config {
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
+
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
@@ -38,13 +42,29 @@ mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
         Rust:
         safe, fast, productive.
-        Pick three.";
+        Pick three.
+        Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+        Rust:
+        safe, fast, productive.
+        Pick three.
+        Trust me.";
+    }
+
+    assert_eq!(
+        vec!["Rust:", "Trust me."],
+        search_case_insensitive(query, contents)
+    );
 }
