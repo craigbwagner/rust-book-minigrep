573f387811e757ea20780dc2cbaaadc0b1cdfb3d
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-05 15:39:02 -0400

    move file reading to run function, return a Box<dyn Error> if there is an error

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{env, fs, process};
+use std::{env, error::Error, fs, process};
 
 struct Config {
     query: String,
@@ -25,6 +25,10 @@ fn main() {
         process::exit(1);
     });
 
-    let contents =
-        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+    run(config);
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.file_path)?;
+    Ok(())
 }
