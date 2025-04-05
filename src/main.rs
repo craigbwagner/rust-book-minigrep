7e1c9f323bb6a5eceae073bdcdc10c3f66b88816
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-05 15:15:21 -0400

    add Config struct and parse config variables from run command arguments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use std::env;
+use std::{env, fs};
+
+struct Config {
+    query: String,
+    file_path: String,
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
+
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
