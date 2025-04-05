2d1b2e35137554ea65a6e4d0826d587ebdcfcca2
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-05 15:25:49 -0400

    move parse_config to be a constructor function for the Config struct

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,20 @@ struct Config {
     file_path: String,
 }
 
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Config { query, file_path }
+    }
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     let contents =
         fs::read_to_string(config.file_path).expect("Should have been able to read the file");
-}
-
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
 }
