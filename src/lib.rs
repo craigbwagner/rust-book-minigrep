81303f107ba01d2e1016877f22c9498129d65de8
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-05 16:13:12 -0400

    add search_case_insensitive function

--- src/lib.rs
+++ src/lib.rs
@@ -37,6 +37,18 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query) {
+            results.push(line.trim());
+        }
+    }
+
+    results
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -61,10 +73,10 @@ mod tests {
         safe, fast, productive.
         Pick three.
         Trust me.";
-    }
 
-    assert_eq!(
-        vec!["Rust:", "Trust me."],
-        search_case_insensitive(query, contents)
-    );
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
+    }
 }
