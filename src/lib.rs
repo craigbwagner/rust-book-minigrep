29668cb6bdf31b844a5a31d4b76acb51e51d3171
craigbwagner <728294+craigbwagner@users.noreply.github.com>
2025-04-12 17:23:36 -0400

    pass iterator into Config::Build instead of array of Strings

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
-use std::{env, error::Error, fs};
+use std::{
+    env::{self},
+    error::Error,
+    fs,
+};
 
 pub struct Config {
     pub query: String,
@@ -7,12 +11,17 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get query string"),
+        };
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
         Ok(Config {
